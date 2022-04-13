9cc3bb97e80241d5f507c1a557ef6237bdc04abc
SKITINA <skitinanouhaila44@gmail.com>
2022-04-13 12:18:39 +0000

    test

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -6,10 +6,12 @@ const ProductCard = ({ product }) => {
   return (
     <Card style={{ width: "18rem" }}>
       <Link to={`/products/${product.id}`}>
-      <Card.Img variant="top" src={product.image} className="h-100" />
+        <Card.Img variant="top" src={product.image} className="h-100" />
       </Link>
       <Card.Body>
-        <Link to={`/products/${product.id}`}><Card.Title className='text-dark'>{product.name}</Card.Title></Link>
+        <Link to={`/products/${product.id}`}>
+          <Card.Title className="text-dark">{product.name}</Card.Title>
+        </Link>
         <Rating rating={product.rating} color="rgb(249, 232, 37)" />
         <Card.Text className="h2">${product.price}</Card.Text>
       </Card.Body>
