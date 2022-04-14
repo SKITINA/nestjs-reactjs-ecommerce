9e098b610afdaf5c3dc94043d593f21d90c08d0f
SKITINA <skitinanouhaila44@gmail.com>
2022-04-14 11:00:46 +0000

    contact us et about

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "../../screens/About";
 import CartScreen from "../../screens/CartScreen";
 import CategoryScreen from "../../screens/CategoryScreen";
+import Contact from "../../screens/Contact";
 import ProductScreen from "../../screens/ProductScreen";
 import Footer from "../Footer";
 import Main from "../Main";
@@ -20,7 +21,7 @@ export const App = () => {
         <Route path="/cart/:productId" element={<CartScreen />}  />
         <Route path="/category" element={<CategoryScreen />}  />
         <Route path="/about" element={<About />}  />
-          
+        <Route path="/contact" element={<Contact />}  />
       </Routes>
       </nav>
       <Footer />
--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -1,11 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import {faFacebook, faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
   return (
    <Container>
-       <h1 style={{fontFamily: "cursive"}}>About us</h1>
-       
+       <h1 style={{fontFamily: "cursive"}} className='text-center my-5'>About us</h1>
+       <h2>This is the best e-commerce store ever</h2>
+       <p style={{lineHeight: '35px'}} className="text-black-50">Dolor sunt consectetur Lorem nostrud. Deserunt adipisicing pariatur reprehenderit ex aute labore anim elit duis amet irure anim. Excepteur dolore exercitation ullamco incididunt laborum aliqua excepteur non eu anim ullamco cupidatat et aute.
+
+Commodo id dolore aliquip anim aliqua culpa proident laborum consectetur tempor. Aliquip est ipsum eu occaecat sit culpa aute aute. Qui cupidatat ad excepteur ut consectetur pariatur ea eiusmod dolore nulla velit qui dolor. Laborum elit pariatur veniam est tempor in cillum nulla proident ut dolor ex eiusmod. Cillum tempor officia sunt amet pariatur deserunt magna adipisicing enim exercitation minim deserunt anim. Excepteur officia nulla dolore labore laborum commodo tempor proident esse. Aute sunt sunt ipsum sunt incididunt dolore magna mollit laboris proident eu sit.</p>
+
+<div className='text-center'>
+<h4>Find us on media</h4>
+    <FontAwesomeIcon icon={faFacebook} className="mx-2 " style={{fontSize:"35px"}}/>
+    <FontAwesomeIcon icon={faLinkedin} className="mx-2 " style={{fontSize:"35px"}}/>
+
+</div>
 
    </Container>
   )
